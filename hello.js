a3b8ec4bbac93259aba122e0acacd0f4b39669f8
Alex Franks <frankofranks2015@gmail.com>
2019-01-28 16:38:20 +0000

    daily 1/28/19

--- hello.js
+++ hello.js
@@ -1,1 +1,16 @@
-console.log("hello, we are here");
+const http = require('http');
+
+http.createServer(function(request, response) {
+    //console.log(request);
+    response.writeHead(200); //status code in header
+    response.write("<html><body><p>Dog is running</p></body></html>"); //Response body
+    setTimeout(function() {
+         response.write("<html><body><p>Dog is done.</p></body></html>");
+         response.end()
+    }, 5000);
+    
+    //response.end(); //Close connection
+    
+}).listen(8080); //listen for connections on this port
+
+console.log('Listening to port 8080');
